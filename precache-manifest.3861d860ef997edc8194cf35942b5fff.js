2f2d839374ae6597d2abf106593f05a4f6b09adb
Jacob Myers <myersjac@umich.edu>
2019-03-07 10:50:45 -0500

    Updates

--- precache-manifest.3861d860ef997edc8194cf35942b5fff.js
+++ precache-manifest.3861d860ef997edc8194cf35942b5fff.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "1e4fc8d99f8e9c8391ad",
+    "revision": "cab93dc09aeae30ccbba",
     "url": "/static/css/main.8deb46fa.chunk.css"
   },
   {
-    "revision": "1e4fc8d99f8e9c8391ad",
-    "url": "/static/js/main.0680bd56.chunk.js"
+    "revision": "cab93dc09aeae30ccbba",
+    "url": "/static/js/main.6c14603b.chunk.js"
   },
   {
     "revision": "fdfcfda2d9b1bf31db52",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/static/media/logo.5d5d9eef.svg"
   },
   {
-    "revision": "b9ff19cbcd103a2c76c0283d4b4307ae",
+    "revision": "e40aa8de46c440e1841f9ac342924584",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c18feedaafc3ddf2a3dbffdcb93f70f0.js"
+  "/precache-manifest.3861d860ef997edc8194cf35942b5fff.js"
 );
 
 workbox.clientsClaim();
