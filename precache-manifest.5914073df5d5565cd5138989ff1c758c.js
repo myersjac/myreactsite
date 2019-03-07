88491ec5ee2669f92d85a8d0d0473702026aae21
Jacob Myers <myersjac@umich.edu>
2019-03-07 10:03:43 -0500

    Updates

---
+++ precache-manifest.5914073df5d5565cd5138989ff1c758c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8217adfa74ad3badde30",
+    "url": "/myreactsite/static/css/main.8deb46fa.chunk.css"
+  },
+  {
+    "revision": "8217adfa74ad3badde30",
+    "url": "/myreactsite/static/js/main.05e7e9f4.chunk.js"
+  },
+  {
+    "revision": "a0d1099e699351b2fa71",
+    "url": "/myreactsite/static/js/runtime~main.b146c422.js"
+  },
+  {
+    "revision": "3791395ae6897930f09c",
+    "url": "/myreactsite/static/js/2.b19c2b74.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myreactsite/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9db9d85c1ed2688ea9404c365f12ceff",
+    "url": "/myreactsite/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e74ec91afc14056146b3ef52d4d7e077.js"
+  "/myreactsite/precache-manifest.5914073df5d5565cd5138989ff1c758c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/myreactsite/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
