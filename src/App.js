ca25eee56d12f99d55ca39d3bb66f84e3ffd60b3
Jacob Myers <myersjac@umich.edu>
2019-03-18 12:23:21 -0400

    template functioning, starting edits

--- src/App.js
+++ src/App.js
@@ -1,29 +1,58 @@
 import React, { Component } from 'react';
-import logo from './JM_LOGO_Vanilla.svg';
+import ReactGA from 'react-ga';
+import $ from 'jquery';
 import './App.css';
-const ScrollReveal = require('scrollreveal');
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import About from './Components/About';
+import Resume from './Components/Resume';
+import Contact from './Components/Contact';
+import Testimonials from './Components/Testimonials';
+import Portfolio from './Components/Portfolio';
 
 class App extends Component {
 
+  constructor(props){
+    super(props);
+    this.state = {
+      foo: 'bar',
+      resumeData: {}
+    };
+
+    ReactGA.initialize('UA-110570651-1');
+    ReactGA.pageview(window.location.pathname);
+
+  }
+
+  getResumeData(){
+    $.ajax({
+      url:'/resumeData.json',
+      dataType:'json',
+      cache: false,
+      success: function(data){
+        this.setState({resumeData: data});
+      }.bind(this),
+      error: function(xhr, status, err){
+        console.log(err);
+        alert(err);
+      }
+    });
+  }
+
+  componentDidMount(){
+    this.getResumeData();
+  }
+
   render() {
     return (
-      <div>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-
-          <h1 class="text1">
-            JAKE MYERS
-          </h1>
-
-          <h3>
-            HI Internet! I'm currently building my site so here's a spinner that will stop once it's done.
-          </h3>
-
-          <p>
-            Thanks to jake rothman for helping choose my domain name! he's gr8!!
-        </p>
-
-        </header>
+      <div className="App">
+        <Header data={this.state.resumeData.main}/>
+        <About data={this.state.resumeData.main}/>
+        <Resume data={this.state.resumeData.resume}/>
+        <Portfolio data={this.state.resumeData.portfolio}/>
+        <Testimonials data={this.state.resumeData.testimonials}/>
+        <Contact data={this.state.resumeData.main}/>
+        <Footer data={this.state.resumeData.main}/>
       </div>
     );
   }
