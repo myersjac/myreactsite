e43e5390f38ded1313c68dff4c8950bc267a4550
Jacob Myers <myersjac@umich.edu>
2019-07-25 10:51:36 -0400

    Updates

---
+++ precache-manifest.9ff874576d0724d36bc335e16e80eeb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f42cdf2212ae704e7d71",
+    "url": "/static/css/main.07318036.chunk.css"
+  },
+  {
+    "revision": "f42cdf2212ae704e7d71",
+    "url": "/static/js/main.cbee8599.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "bb352f5756a5898e4ff9",
+    "url": "/static/css/2.53fdebde.chunk.css"
+  },
+  {
+    "revision": "bb352f5756a5898e4ff9",
+    "url": "/static/js/2.278ef06d.chunk.js"
+  },
+  {
+    "revision": "5ff154ca40fd3606a676151876617a07",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f5bacdfac81df85c616d902c26d50c92.js"
+  "/precache-manifest.9ff874576d0724d36bc335e16e80eeb3.js"
 );
 
 workbox.clientsClaim();
