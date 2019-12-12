2c6aae369b023d1a762335ff6c0561ab557799f0
Jacob Myers <jacobmyers@Deej-M-a-c-c.local>
2019-12-11 23:20:35 -0500

    Updates

---
+++ precache-manifest.ac8f472cfd06665bf6cdcfa4d8d98b98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e762731bff8bdf675c06662698b6c0d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38c02312650389c0a68f",
+    "url": "/static/css/2.a881067f.chunk.css"
+  },
+  {
+    "revision": "039e915bc0b2235a3bd1",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "38c02312650389c0a68f",
+    "url": "/static/js/2.841586b6.chunk.js"
+  },
+  {
+    "revision": "13692df382ebf1732630eb134b7e2896",
+    "url": "/static/js/2.841586b6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "039e915bc0b2235a3bd1",
+    "url": "/static/js/main.a7b377aa.chunk.js"
+  },
+  {
+    "revision": "324a3b33c4eced464a23",
+    "url": "/static/js/runtime-main.4852e78f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.60e67332d4fe761e5c1cee98670547c1.js"
+  "/precache-manifest.ac8f472cfd06665bf6cdcfa4d8d98b98.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
